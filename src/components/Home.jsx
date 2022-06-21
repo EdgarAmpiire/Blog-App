d4c7173672a835a821c98afd2385c23a283814ce
Ed-Mukundane <aedgar990@gmail.com>
2022-06-22 01:21:46 +0300

    deleting blog

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,42 @@
-import React from "react"; 
+/** @format */
+
+import React from "react";
+import { BlogList } from "./BlogList";
 
 export function Home() {
-    return (
-        <>
-            <div className="mx-25 px-8 mt-10">
-                <h1 className="text-2xl text-black">Homepage</h1>
-        </div>
-        </>
-    )
+	const [blogs, setBlogs] = React.useState([
+		{
+			title: "Welcome Party!",
+			body: "lorem ipsum...",
+			author: "John Doe",
+			id: 1,
+		},
+		{
+			title: "Birthday Girl",
+			body: "lorem ipsum...",
+			author: "Joseph Bright",
+			id: 2,
+		},
+		{
+			title: "The Dev",
+			body: "lorem ipsum...",
+			author: "Jonthan Saxton",
+			id: 3,
+		},
+	]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter((blog) => blog.id !== id);
+        setBlogs(newBlogs);
+
+        // console.log("delete wworking")
+    }
+
+	return (
+		<>
+			<div className="mx-25 px-8 mt-10">
+				<BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete} />
+			</div>
+		</>
+	);
 }
