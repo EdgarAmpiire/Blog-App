3b9ed744ed3b107b32e6a6b1405b7a473b1d569c
Ed-Mukundane <aedgar990@gmail.com>
2022-06-27 22:57:08 +0300

    handling errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,14 @@
 
 import React, { useEffect } from "react";
 import { BlogList } from "./BlogList";
+import ErrorIcon from "@mui/icons-material/Error";
 
 export function Home() {
 	const [blogs, setBlogs] = React.useState(null);
 
 	const [isLoading, setIsLoading] = React.useState(true)
+
+	const [error, setError] = React.useState(null);
 
     // const handleDelete = (id) => {
     //     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -16,21 +19,37 @@ export function Home() {
 	useEffect(() => {
 		setTimeout(() => {
 			fetch("http://localhost:8000/blogs")
-			.then((res) => {
+				.then((res) => {
+					if (!res.ok) {
+					throw Error("Could not fetch data for that resource!")
+				}
 				return res.json();
 			})
 			.then((data) => {
 				setBlogs(data)
 				setIsLoading(false)
+				setError(null);
 			})
-		}, 1000)
+				.catch(err => {
+					setError(err.message);
+					setIsLoading(false);
+			})
+		}, 1000);
 	}, [])
 
 	return (
 		<>
 			<div className="mx-25 px-8 mt-10">
+				{error && (
+					<div className="text-red-500 flex  gap-1">
+						<div>
+							<ErrorIcon />
+						</div>
+						<div>{error}</div>
+					</div>
+				)}
 				{isLoading && <div>Loading...</div>}
-				{blogs && <BlogList blogs={blogs} title={"All Blogs!"}  />}
+				{blogs && <BlogList blogs={blogs} title={"All Blogs!"} />}
 			</div>
 		</>
 	);
