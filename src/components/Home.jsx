a1f4520c3fd3a2c28d83ee4a8a688d76c4bc80c3
Edgar <aedgar990@gmail.com>
2022-11-25 13:00:04 +0300

    fronend: fetching data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,32 @@
 /** @format */
 
-import React from "react";
+import React, {useEffect} from "react";
 import { BlogList } from "./BlogList";
 import ErrorIcon from "@mui/icons-material/Error";
-import useFetch from "./useFetch";
+// import useFetch from "./useFetch";
 
 export function Home() {
-	const {data:blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
+	// const {data:blogs, isLoading, error } = useFetch("http://localhost:8000/blogs");
 
+	useEffect(() => {
+		const fetchBlogs = async () => {
+			
+		}
+	})
+ 
 	return (
 		<>
 			<div className="mx-25 px-8 mt-10">
-				{error && (
+				{/* {error && (
 					<div className="text-red-500 flex  gap-1">
 						<div>
 							<ErrorIcon />
 						</div>
 						<div>{error}</div>
 					</div>
-				)}
-				{isLoading && <div>Loading...</div>}
-				{blogs && <BlogList blogs={blogs} title={"All Blogs!"} />}
+				)} */}
+				{/* {isLoading && <div>Loading...</div>}
+				{blogs && <BlogList blogs={blogs} title={"All Blogs!"} />} */}
 			</div>
 		</>
 	);
