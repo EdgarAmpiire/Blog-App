f0e32b2492e1678310a5d9f33b8f308d3c1bf461
Edgar <aedgar990@gmail.com>
2022-11-25 13:36:33 +0300

    frontend: fetching data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,22 @@
 /** @format */
 
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import { BlogList } from "./BlogList";
 import ErrorIcon from "@mui/icons-material/Error";
 // import useFetch from "./useFetch";
 
 export function Home() {
 	// const {data:blogs, isLoading, error } = useFetch("http://localhost:4000/api/blogs");
+	const [blogs, setBlogs] = useState(null)
 
 	useEffect(() => {
 		const fetchBlogs = async () => {
 			const response = await fetch('http://localhost:4000/api/blogs')
+			const json = await response.json()
+
+			if(response.ok) {
+
+			}
 		}
 
 		fetchBlogs()
