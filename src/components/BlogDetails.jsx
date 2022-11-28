853c236243207b0d3d8326f08c2b14139bd7861f
Edgar <aedgar990@gmail.com>
2022-11-28 16:59:31 +0300

    fetching single blog

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 export function BlogDetails() {
+	
+	const { id } = useParams(); 
 
 	const [blog, setBlog] = useState(null)
 
 	useEffect(() => {
 		const fetchBlog = async () => {
-			const response = await fetch("/api/blogs/:id")
+			const response = await fetch(`/api/blogs/${id}`)
 			const json = await response.json()
 
 			if (response.ok){
@@ -18,13 +20,13 @@ export function BlogDetails() {
 		fetchBlog()
 	}, [])
 
-    const { id } = useParams();
 
     return (
 			<>
 				<div className="mx-25 px-8 mt-10">
                 <h1 className="text-xl text-black">Blog Details - {id} </h1>
 				
+
 				</div>
 			</>
 		);
